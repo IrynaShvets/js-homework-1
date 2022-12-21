23d7a120cb8282f7c24a64e434d0e1a9976ea97a
IrynaShvets <86491334+IrynaShvets@users.noreply.github.com>
2022-12-21 16:17:20 +0200

    test

---
+++ index.js
@@ -0,0 +1,58 @@
+const num = 10;
+console.log(num);
+
+const first = 5;
+const second = 10;
+
+console.log(first + second);
+console.log(first - second);
+console.log(first * second);
+console.log(first / second);
+
+const a = 3;
+const b = 10;
+const result = a + b;
+console.log(result);
+
+const bar = 20;
+const qwerty = 5;
+const res = (bar - qwerty) - 10;
+console.log(res);
+
+const end = res - qwerty;
+console.log(end);
+
+const str = "Hello, Palmo";
+alert(str);
+
+const name = "Ірина";
+const age = 38;
+alert(`Я ${name}, мені ${age}.`);
+
+
+const seconds = 60; 
+const hour = () => {
+   return `${seconds}` * 60;
+};
+console.log(hour());
+
+const day = () => {
+    return `${seconds}` * 60 * 24;
+ };
+console.log(day());
+
+const month = () => {
+    return `${seconds}` * 60 * 24 * 30;
+ };
+console.log(month());
+
+const number = 9;
+console.log(Math.pow(9, 2));
+
+
+const d = 8;
+const n = Math.pow(`${d}`, 2)
+console.log(n);
+
+const x = Math.pow(`${d}`, 3)
+console.log(x);
